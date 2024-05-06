3f1cffd18ee72a5ecbf9970e719148f2aedb6993
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-05-05 20:44:39 -0600

    se agrego codigo para crear una carpeta si no existe y dentro de la carpeta guardar el proto

--- factory/maker.js
+++ factory/maker.js
@@ -2,10 +2,16 @@ const file = require('fs')
 
 const makeProto=async(nombre)=>{
 
+    const ruta = './protos'
+
+    if(!file.existsSync(ruta)){
+        file.mkdirSync(ruta)
+    }
+
     const body=`{\n "name":"${nombre}",\n "colums":[]\n}`
 
     try{
-        file.writeFileSync(`${nombre}.json`,body)
+        file.writeFileSync(`./protos/${nombre}.json`,body)
         return `${nombre}.json`
     }
     catch(error){
