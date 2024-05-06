ab3de404da699e1fc85da1568b0862613be0401e
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-05-05 20:58:27 -0600

    se agrego un prototipo para la importacion de los protos creados

--- sqlSteve.js
+++ sqlSteve.js
@@ -3,6 +3,12 @@ const {makeProto}=require('./factory/maker')
 console.clear()
 console.log('sqlSteve')
 
+const fileName='prueba.json'
+
+//const archivo = require(`./protos/${fileName}`)
+
+//console.log(archivo)
+
 makeProto('prueba')
     .then(nombre=>console.log(nombre, 'creado \n happy hacking!'))
     .catch(err=>console.log(err))
