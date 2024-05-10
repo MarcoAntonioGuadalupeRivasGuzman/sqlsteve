33d61cd45ece3c5ec5ac22be27dcba03ce52a361
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-05-09 21:31:51 -0600

    se agrego una variable desde yargs para leer el proto

--- sqlSteve.js
+++ sqlSteve.js
@@ -1,13 +1,14 @@
 const {makeProto}=require('./factory/maker')
+const argv = require('yargs').argv;
 
 console.clear()
 console.log('sqlSteve')
 
-const fileName='prueba.json'
+const fileName=argv.proto
 
-//const archivo = require(`./protos/${fileName}`)
+const archivo = require(`./protos/${fileName}`)
 
-//console.log(archivo)
+console.log(archivo)
 
 makeProto('prueba')
     .then(nombre=>console.log(nombre, 'creado \n happy hacking!'))
