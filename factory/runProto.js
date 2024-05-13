4154062c8179bd513ea91a8bf86137bfb1ef9f8c
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-05-12 21:37:41 -0600

    se agrego una funcion para agregar la sentencia segun el tipo de dato del campo

--- factory/runProto.js
+++ factory/runProto.js
@@ -1,7 +1,36 @@
 
 import { readFile } from 'fs/promises';
 
-export const  runProto=async(fileName)=>{
+function setFieldType(campo) {
+
+    switch (campo.typeField) {
+
+        case 'int':
+        case 'bigint':
+        case 'bool':
+        case 'bolean':
+        case 'date':
+            return `${campo.nameField} ${campo.typeField}`
+
+        case 'float':
+        case 'double':
+        case 'decimal':
+            return `${campo.nameField} ${campo.typeField}(${campo.sizeField},${campo.floatField})`
+
+        case 'char':
+        case 'varchar':
+        case 'binary':
+        case 'varbinary':
+        case 'bit':
+        case 'tinyint':
+        case 'smallint':
+        case 'mediumint':  
+            return `${campo.nameField} ${campo.typeField}(${campo.sizeField})`
+
+    }
+}
+
+export const runProto = async (fileName) => {
 
     const jsonContent = await readFile(`./protos/${fileName}`, 'utf-8');
     const proto = JSON.parse(jsonContent);
@@ -10,14 +39,15 @@ export const  runProto=async(fileName)=>{
 
     let sentencia = ''
 
-    sentencia='create table '+proto.name+'(id primary key,'
+    sentencia = 'create table ' + proto.name + '(id primary key,'
 
-    proto.colums.forEach(function(columna){
-        sentencia+=columna.nameField+' '+columna.typeField+' '
+    proto.colums.forEach(function (columna,index) {
+        sentencia += setFieldType(columna)
+        index === proto.colums.length-1 ? sentencia+=');' : sentencia+=','
     })
 
 
-    console.log(sentencia)
+    //console.log(sentencia)
 
     return sentencia
 
