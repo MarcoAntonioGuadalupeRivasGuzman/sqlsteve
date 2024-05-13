08e9a3ace6771a887564ab4652d334451b153be3
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-05-12 19:13:06 -0600

    se creo un menu iteractivo con las flechas

--- sqlSteve.js
+++ sqlSteve.js
@@ -1,10 +1,14 @@
-const {makeProto}=require('./factory/maker')
-const argv = require('yargs').argv;
+/*const {makeProto}=require('./factory/maker')
+const argv = require('yargs').argv;*/
+
+import { makeProto } from "./factory/maker.js"
+import { menu } from "./helper/menu.js"
+import inquirer from 'inquirer'
 
 console.clear()
 console.log('sqlSteve')
 
-const fileName=argv.proto
+/*const fileName=argv.proto
 
 const archivo = require(`./protos/${fileName}`)
 
@@ -12,6 +16,30 @@ console.log(archivo)
 
 makeProto('prueba')
     .then(nombre=>console.log(nombre, 'creado \n happy hacking!'))
-    .catch(err=>console.log(err))
+    .catch(err=>console.log(err))*/
 
 
+const opcion = await inquirer.prompt([
+    {
+        type: 'list',
+        name: 'valor',
+        message: 'Seleciona una opcion: 1.Crear proto 2.lista protos 3.salir ',
+        choices: [1, 2, 3]
+    }
+])
+
+//console.log(opcion.valor)
+
+switch (opcion.valor) {
+    case 1:
+        const nombre = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'valor',
+                message: 'Ingresa el nombre: ',
+            }
+        ])
+
+        console.log(makeProto(nombre.valor), 'creado \n happy hacking!')
+        break
+}
