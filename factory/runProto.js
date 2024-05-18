618f66bb066743d2ee5f85d59d9584589aa50e99
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-05-18 17:51:00 -0600

    se quito una impresion que no tenia sentido

--- factory/runProto.js
+++ factory/runProto.js
@@ -39,7 +39,7 @@ export const runProto = async (fileName) => {
     const jsonContent = await readFile(`./protos/${fileName}`, 'utf-8');
     const proto = JSON.parse(jsonContent);
 
-    console.log(proto.name)
+    //console.log(proto.name)
 
     let sentencia = ''
 
