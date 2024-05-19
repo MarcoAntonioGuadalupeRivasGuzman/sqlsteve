f0a9852051740f303b0bd91079cadea502d7a519
Marco <za19012397@zapopan.tecmm.edu.mx>
2024-05-19 09:30:55 -0600

    se formateo el codigo para que se vea mejor en la clase

--- dataObjects/prueba.js
+++ dataObjects/prueba.js
@@ -1,8 +1,8 @@
 class prueba {
-constructor(nombre,edad,fecha,altura) {
-this.nombre = nombre
-this.edad = edad
-this.fecha = fecha
-this.altura = altura
-}
+	constructor(nombre,edad,fecha,altura) {
+		this.nombre = nombre
+		this.edad = edad
+		this.fecha = fecha
+		this.altura = altura
+	}
 }
